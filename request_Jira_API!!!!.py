997c3d27acd601e36e9d0fa0a32c1c0f14abe7ed
brazhelovich <47822663+brazhelovich@users.noreply.github.com>
2020-01-03 17:54:04 +0300

    Create request_Jira_API!!!!.py

---
+++ request_Jira_API!!!!.py
@@ -0,0 +1,21 @@
+import requests
+import json
+def test_request():
+    #response = requests.get('http://10.6.211.178:8080//jira/plugins/servlet/applications/versions-licenses', auth=('admin', 123456))
+    response = requests.get('http://10.6.211.178:8080/jira/rest/api/2/issue/PAD-1', auth=('admin', 123456))
+    #with open("data_file.json", "w") as write_file:
+        #json.dump(response, write_file)
+    #data = json.dumps(response)
+    #data = print(response.json().dumps
+    #print(response)
+    data = response.content.decode('UTF-8')
+    dat = json.loads(data.)['fields']['issuetype']['description']
+
+
+    #print(data['sandwich'])
+
+
+    print(dat)
+    #return response
+
+test_request()
